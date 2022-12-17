635798d2f000e7e7285dd829dd05a6ce8af928e4
neumanf <fabricionewman@protonmail.com>
2022-12-17 14:13:48 -0300

    feat: show state and city detailts in highest price analysis

--- apps/highest_price.py
+++ apps/highest_price.py
@@ -1,3 +1,4 @@
+from pyspark.sql import functions as F
 from init_spark import init_spark
 
 spark = init_spark()
@@ -8,9 +9,14 @@ df = spark.read.format("mongodb").load()
 df = df.filter(df['produto'] == 'GASOLINA')
 
 # Mapeia e converte valores do produto de string para float
-df = df.rdd.map(lambda x: (x.produto, float(x.valor_de_venda.replace(",", ".")))).toDF(["produto", "valor"])
+df = df.rdd \
+    .map(lambda x: (x.estado, x.municipio, x.produto, float(x.valor_de_venda.replace(",", ".")))) \
+    .toDF(["estado", "municipio", "produto", "valor"])
 
 # Obtem o valor maximo
-df = df.groupBy("produto").max("valor")
+df = df.groupBy("produto") \
+    .agg(F.max("valor"), \
+        F.expr("max_by(estado, valor) as estado"), \
+        F.expr("max_by(municipio, valor) as municipio"))
 
 df.show()
